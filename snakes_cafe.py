1581e932c835bad8c18da57242644e5afb01a0ab
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-03-20 15:12:19 -0700

    refactor into functions

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -8,30 +8,35 @@ menu = {
 
 user_order = []
 
-if __name__ == "__main__":
-    print('**************************************\n** Welcome to the Snakes\
+print('**************************************\n** Welcome to the Snakes\
 Cafe! **\n** Please see our menu below. **\n** To quit at any time,\
 type "quit" **\n**************************************')
 
-for key, values in menu.items():
-    for value in values:
-        print(value)
+def print_values():
+    for values in menu.values():
+        for value in values:
+            print(value)
 
 user_input = input('***********************************\n** What would you\
  like to order? ''**\n***********************************\n')
 
-# continue_order = input('***********************************\n** Are you finished with\
-# your order? Type "quit" if yes''**\n***********************************\n')
+continue_order = input('***********************************\n** Are you finished with\
+your order? Type "quit" if yes''**\n***********************************\n')
 
-while True:
-    continue_order = input('***********************************\n** Are you finished\
-with your order? Type "quit" if yes''**\n***********************************\n')
-    if continue_order in menu.items():
-        user_order.append(continue_order)
-        print('Your current order consists of ' + str(user_order))
-        pass
-    else:
-        continue_order = 'quit()'
+def user_input_to_list():
+    while True:
+        continue_order
+        if continue_order in menu.items():
+            user_order.append(continue_order)
+            print('Your current order consists of ' + str(user_order))
+            pass
+
+def user_quit():
+    if continue_order == 'quit()':
         print('***********************************\n** Your order has been completed\
         ''**\n***********************************\n')
-        break
+
+if __name__ == "__main__":
+    print_values()
+    user_input_to_list()
+    user_quit()
