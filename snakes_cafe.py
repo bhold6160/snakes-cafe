f340af6590e3047a37086dd1318c624c5db21efb
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-03-20 14:04:10 -0700

    commit changes

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,5 +1,5 @@
 menu = {
-    'appetizers' : ['Appetizers:','----------', 'Wings', 'Cookies', 'Spring Rolls\n'],
+    'appetizers' : ['Appetizers:','- *2', 'Wings', 'Cookies', 'Spring Rolls\n'],
 'entrees' : ['Entrees:', '----------','Salmon',
            'Steak', 'Meat Tornado', 'A Literal Garden\n'],
 'desserts' : ['Desserts:', '----------', 'Ice Cream', 'Cake', 'Pie\n'],
@@ -8,16 +8,17 @@ menu = {
 
 user_order = []
 
-print('**************************************\n** Welcome to the Snakes\
-Cafe! **\n** Please see our menu below. **\n** To quit at any time, \
-type "quit" **\n**************************************')
+if __name__ == "__main__":
+    print('**************************************\n** Welcome to the Snakes\
+    Cafe! **\n** Please see our menu below. **\n** To quit at any time, \
+    type "quit" **\n**************************************')
 
 for key, values in menu.items():
     for value in values:
         print(value)
 
-# user_input = input('***********************************\n** What would you\
-#  like to order? ''**\n***********************************\n')
+user_input = input('***********************************\n** What would you\
+ like to order? ''**\n***********************************\n')
 
 # continue_order = input('***********************************\n** Are you finished with\
 # your order? Type "quit" if yes''**\n***********************************\n')
@@ -25,14 +26,14 @@ for key, values in menu.items():
 # print(user_input)
 
 while True:
-    user_input = input('***********************************\n** What would you\
- like to order? ''**\n***********************************\n')
-    if user_input in menu.items():
-        user_order.append(user_input)
+    continue_order = input('***********************************\n** Are you finished\
+    with your order? Type "quit" if yes''**\n***********************************\n')
+    if continue_order in menu.items():
+        user_order.append(continue_order)
         print('Your current order consists of ' + str(user_order))
         pass
     else:
-        user_input = 'quit()'
+        continue_order = 'quit()'
         print('***********************************\n** Your order has been completed\
         ''**\n***********************************\n')
         break
