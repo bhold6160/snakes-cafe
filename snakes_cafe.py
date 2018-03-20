db31be641f21a631b0e98dd7f350332512176cd3
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-03-20 16:18:52 -0700

    code runs but not functioning correctly

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -8,35 +8,49 @@ menu = {
 
 user_order = []
 
-print('**************************************\n** Welcome to the Snakes\
+def welcome_message():
+    print('**************************************\n** Welcome to the Snakes\
 Cafe! **\n** Please see our menu below. **\n** To quit at any time,\
 type "quit" **\n**************************************')
+
+class print_messages:
+    def user_input(self):
+        self.initial_input = input('***********************************\n** What would you\
+ like to order? ''**\n***********************************\n')
+
+    def continue_order(self):
+        self.continue_order_input = input('***********************************\n** Are you finished with\
+your order? Type "quit" if yes''**\n***********************************\n>')
+
+p = print_messages()
 
 def print_values():
     for values in menu.values():
         for value in values:
             print(value)
 
-user_input = input('***********************************\n** What would you\
- like to order? ''**\n***********************************\n')
+# def user_input():
+#     initial_input = input('***********************************\n** What would you\
+#  like to order? ''**\n***********************************\n')
 
-continue_order = input('***********************************\n** Are you finished with\
-your order? Type "quit" if yes''**\n***********************************\n')
+#     continue_order = input('***********************************\n** Are you finished with\
+# your order? Type "quit" if yes''**\n***********************************\n>')
 
 def user_input_to_list():
     while True:
-        continue_order
-        if continue_order in menu.items():
-            user_order.append(continue_order)
+        p.continue_order
+        if p.continue_order in menu.items():
+            user_order.append(p.continue_order)
             print('Your current order consists of ' + str(user_order))
-            pass
 
 def user_quit():
-    if continue_order == 'quit()':
+    if p.continue_order == 'quit':
         print('***********************************\n** Your order has been completed\
         ''**\n***********************************\n')
 
 if __name__ == "__main__":
+    welcome_message()
     print_values()
+    p.continue_order()
     user_input_to_list()
     user_quit()
