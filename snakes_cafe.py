c71656cd36d5d3db33e093329acec09f502db16c
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-03-20 00:56:18 -0700

    reduced code

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,32 +1,24 @@
-appetizers = ['Appetizers:\n','----------', 'Wings', 'Cookies', 'Spring Rolls\n']
-entrees = ['Entrees:', '----------','Salmon',
-           'Steak', 'Meat Tornado', 'A Literal Garden\n']
-desserts = ['Desserts:', '----------', 'Ice Cream', 'Cake', 'Pie\n']
-drinks = ['Drinks', '----------', 'Coffee', 'Tea', 'Blood of the Innocent\n']
+menu = {
+    'appetizers' : ['Appetizers:\n','----------', 'Wings', 'Cookies', 'Spring Rolls\n'],
+'entrees' : ['Entrees:', '----------','Salmon',
+           'Steak', 'Meat Tornado', 'A Literal Garden\n'],
+'desserts' : ['Desserts:', '----------', 'Ice Cream', 'Cake', 'Pie\n'],
+'drinks' : ['Drinks', '----------', 'Coffee', 'Tea', 'Blood of the Innocent\n'],
+}
 
 user_order = []
 print('**************************************\n** Welcome to the Snakes\
 Cafe! **\n** Please see our menu below. **\n** To quit at any time, \
 type "quit" **\n**************************************')
 
-for item in appetizers:
-    print(item)
+for key, values in menu.items():
+    for value in values:
+        print(value)
 
-for item in entrees:
-    print(item)
-
-for item in desserts:
-    print(item)
-
-for item in drinks:
-    print(item)
-
-# user_quit = 'quit()'
 while True:
     user_input = input('***********************************\n** What would you\
      like to order? ''**\n***********************************\n')
-    if user_input in drinks or user_input in desserts or user_input in entrees\
-         or user_input in appetizers:
+    if user_input in menu:
         user_order.append(user_input)
         print('Your current order consists of ' + str(user_order))
         continue
