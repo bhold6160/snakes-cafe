cd825aac1ed2249c5c019f651301efa798bc3f9b
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-03-20 15:13:33 -0700

    changed around the __name__ placement

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -8,10 +8,7 @@ menu = {
 
 user_order = []
 
-if __name__ == "__main__":
-    print('**************************************\n** Welcome to the Snakes\
-Cafe! **\n** Please see our menu below. **\n** To quit at any time,\
-type "quit" **\n**************************************')
+
 
 for key, values in menu.items():
     for value in values:
@@ -20,12 +17,15 @@ for key, values in menu.items():
 user_input = input('***********************************\n** What would you\
  like to order? ''**\n***********************************\n')
 
+
+
 # continue_order = input('***********************************\n** Are you finished with\
 # your order? Type "quit" if yes''**\n***********************************\n')
 
 while True:
     continue_order = input('***********************************\n** Are you finished\
 with your order? Type "quit" if yes''**\n***********************************\n')
+
     if continue_order in menu.items():
         user_order.append(continue_order)
         print('Your current order consists of ' + str(user_order))
@@ -35,3 +35,8 @@ with your order? Type "quit" if yes''**\n***********************************\n')
         print('***********************************\n** Your order has been completed\
         ''**\n***********************************\n')
         break
+
+if __name__ == "__main__":
+    print('**************************************\n** Welcome to the Snakes\
+    Cafe! **\n** Please see our menu below. **\n** To quit at any time,\
+    type "quit" **\n**************************************')
