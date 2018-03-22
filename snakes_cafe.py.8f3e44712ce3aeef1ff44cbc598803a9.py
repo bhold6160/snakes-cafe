f3bc56bdc5fcdfbc34ff13f901961fe22d856543
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-03-22 15:59:31 -0700

    printing order receipt correctly

--- snakes_cafe.py.8f3e44712ce3aeef1ff44cbc598803a9.py
+++ snakes_cafe.py.8f3e44712ce3aeef1ff44cbc598803a9.py
@@ -2,44 +2,44 @@ import uuid
 
 menu = {
     'Appetizers': {
-        'Wings': 8.00 ,
-        'Spring Rolls': 5.00 ,
-        'Cookies': 2.00 ,
-        'Grilled Squid': 8.00 ,
-        'Crab Wonton': 6.00 ,
-        'Satay': 7.00 
+        'Wings': [8.00, 0],
+        'Spring Rolls': [5.00, 0],
+        'Cookies': [2.00, 0],
+        'Grilled Squid': [8.00, 0],
+        'Crab Wonton': [6.00, 0],
+        'Satay': [7.00, 0]
     },
     'Entrees': {
-        'Salmon': 15.00 ,
-        'Steak': 20.00 ,
-        'Meat Tornado': 25.00 ,
-        'A Literal Garden': 12.00 ,
-        'Pad Thai': 10.00 ,
-        'Spicy Meatballs': 12.00 
+        'Salmon': [15.00, 0],
+        'Steak': [20.00, 0],
+        'Meat Tornado': [25.00, 0],
+        'A Literal Garden': [12.00, 0],
+        'Pad Thai': [10.00, 0],
+        'Spicy Meatballs': [12.00, 0]
     },
     'Desserts': {
-        'Ice Cream': 6.00 ,
-        'Cake': 6.00 ,
-        'Pie': 7.00 ,
-        'Mango Sicky Rice': 6.00 ,
-        'Mushroom Yogurt': 5.00 ,
-        'Popsicle': 3.00 
+        'Ice Cream': [6.00, 0],
+        'Cake': [6.00, 0],
+        'Pie': [7.00, 0],
+        'Mango with Sicky Rice': [6.00, 0],
+        'Mushroom Yogurt': [5.00, 0],
+        'Popsicle': [3.00, 0]
     },
     'Drinks': {
-        'Coffee': 3.00 ,
-        'Tea': 2.00 ,
-        'Innocent Blood': 50.00 ,
-        'Champagne': 8.00 ,
-        'Martini': 11.00 ,
-        'Italian Lemondrop': 10.00 
+        'Coffee': [3.00, 0],
+        'Tea': [2.00, 0],
+        'Blood of the Innocent': [50.00, 0],
+        'Champagne': [8.00, 0],
+        'Martini': [11.00, 0],
+        'Italian Lemondrop': [10.00, 0]
     },
     'Sides': {
-        'Bread': 2.00 ,
-        'Hot Peppers': 1.00 ,
-        'Potatoes': 3.00 ,
-        'Bacon': 5.00 ,
-        'Apples': 1.00 ,
-        'Rice': 4.00 
+        'Bread': [2.00, 0],
+        'Hot Peppers': [1.00, 0],
+        'Potatoes': [3.00, 0],
+        'Bacon': [5.00, 0],
+        'Apples': [1.00, 0],
+        'Rice': [4.00, 0]
     }
 }
 
@@ -64,10 +64,10 @@ def print_menu():
         '''
         for item, price in value.items():
             item_str = item.ljust(20)
-            price_str = ('$' + str(price) + '0').rjust(15)
+            price_str = ('$' + str(price[0]) + '0').rjust(15)
             new_str = item_str + price_str
             print(new_str)
-            return_value += '{}: ${p:0.2f}\n' .format(item, p=price)
+            return_value += '{}: ${p:0.2f}\n' .format(item, p=price[0])
     return return_value
 
 
@@ -104,16 +104,16 @@ def check_user_input():
 def print_order(user_order):
     order_summary = 'Order #{}\n'.format(uuid.uuid4())
     for item, quantity in user_order.items():
-        item = item.title()
         for category in menu.values():
-            if item in category:
-                order_summary += '\n{}: {} ${}'.format(quantity, item, category[item])
+            import pdb; pdb.set_trace()
+            if item in item.lower() for item in menu[course]]category:
+                order_summary += '\n{}: {} total: {}'.format(item, quantity, category)
     return order_summary
 
 
 def add_order(item):
     for course in menu:
-        if item.title() in menu[course]:
+        if item in [item.lower() for item in menu[course]]:
             if item in user_order:
                 user_order[item] = user_order[item] + 1
             else:
